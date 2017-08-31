285f6d9dd32f1c048d6ed7e98d03da67c7113d07
wangqingchen <wangqc@zetyun.com>
2017-08-31 13:20:20 +0800

    init backend project

---
+++ backend/src/main/java/com/dli/SmileApplication.java
@@ -0,0 +1,18 @@
+package com.dli;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class SmileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmileApplication.class, args);
+    }
+
+}
---
+++ backend/src/main/java/com/dli/controllers/HomeController.java
@@ -0,0 +1,25 @@
+package com.dli.controllers;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+    @RequestMapping("")
+    public String index() {
+        return "smile backend service is running...";
+    }
+
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public Map add(@RequestBody Map body){
+        System.out.print(body);
+        return body;
+    }
+}
