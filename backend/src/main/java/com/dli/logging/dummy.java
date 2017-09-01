913efe0cd707b0a54a98ac80db8eef17b4b78fe7
wangqingchen <wangqc@zetyun.com>
2017-09-01 14:08:27 +0800

    add dummy classes

---
+++ backend/src/main/java/com/dli/logging/dummy.java
@@ -0,0 +1,7 @@
+package com.dli.logging;
+
+/**
+ * Created by wangqc on 9/1/17.
+ */
+public class dummy {
+}
---
+++ backend/src/main/java/com/dli/repositories/dummy.java
@@ -0,0 +1,7 @@
+package com.dli.repositories;
+
+/**
+ * Created by wangqc on 9/1/17.
+ */
+public class dummy {
+}
