a913363b0275ec4cf1cd48a58c50a4b8f6482fb6
wangqingchen <wangqc@zetyun.com>
2017-09-16 21:56:22 +0800

    change course controller.

--- backend/src/main/java/com/dli/controllers/CourseController.java
+++ backend/src/main/java/com/dli/controllers/CourseController.java
@@ -5,7 +5,7 @@ import com.dli.helper.Constant;
 import com.dli.helper.Helper;
 import com.dli.services.CollectService;
 import com.dli.services.CourseService;
-import com.dli.services.DemoService;
+import com.dli.services.LogonService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,215 +29,302 @@ public class CourseController {
     @Autowired
     private CollectService collectService;
 
+    @Autowired
+    private LogonService logonService;
+
 
     @RequestMapping(value = "/getCategoryList", method = RequestMethod.GET)
-    public Map getCategoryList(int skip, int take) {
-        try {
-            Map<String, Object> result = new HashMap<String, Object>();
+    public Map getCategoryList(int skip, int take, @RequestHeader Map header) {
+        Map<String, Object> result = new HashMap<String, Object>();
+        String token = header.get("token").toString();
+        User user = logonService.getUserByToken(token);
+        if (user == null) {
+            result.put(Constant.status, 0);
+            result.put(Constant.result, "无效的登录用户");
+            return result;
+        }
 
-            List<CourseCategory> lst = courseService.getCourseCategoryList(Helper.GetCurrentUser().getCompany_id(), skip, take);
+        try {
+
+            List<CourseCategory> lst = courseService.getCourseCategoryList(user.getCompany_id(), skip, take);
 
             result.put(Constant.status, 1);
             result.put(Constant.result, lst);
 
-            return result;
+
 
         } catch (Exception ex) {
             logger.error(ex.getMessage());
-            throw ex;
+            result.put(Constant.status, 0);
+            result.put(Constant.result, ex.getMessage());
         }
+        return result;
     }
 
     @RequestMapping(value = "/getCoursesList", method = RequestMethod.GET)
-    public Map getCoursesList(int cateid, int skip, int take) {
-        try {
-            Map<String, Object> result = new HashMap<String, Object>();
+    public Map getCoursesList(int cateid, int skip, int take, @RequestHeader Map header) {
+        Map<String, Object> result = new HashMap<String, Object>();
+        String token = header.get("token").toString();
+        User user = logonService.getUserByToken(token);
+        if (user == null) {
+            result.put(Constant.status, 0);
+            result.put(Constant.result, "无效的登录用户");
+            return result;
+        }
 
-            List<Course> lst = courseService.getCourseList(Helper.GetCurrentUser().getUser_id(), cateid, skip, take);
+        try {
+            List<Course> lst = courseService.getCourseList(user.getUser_id(), cateid, skip, take);
             result.put(Constant.status, 1);
             result.put(Constant.result, lst);
-            return result;
 
         } catch (Exception ex) {
             logger.error(ex.getMessage());
-            throw ex;
+            result.put(Constant.status, 0);
+            result.put(Constant.result, ex.getMessage());
         }
+        return result;
     }
 
     @RequestMapping(value = "/getCourseByID", method = RequestMethod.GET)
-    public Map getCourseByID(int courseid) {
+    public Map getCourseByID(int courseid, @RequestHeader Map header) {
+        Map<String, Object> result = new HashMap<String, Object>();
+        String token = header.get("token").toString();
+        User user = logonService.getUserByToken(token);
+        if (user == null) {
+            result.put(Constant.status, 0);
+            result.put(Constant.result, "无效的登录用户");
+            return result;
+        }
+
         try {
-            Map<String, Object> result = new HashMap<String, Object>();
-            Course c = courseService.getCourseByID(courseid, Helper.GetCurrentUser().getUser_id());
+            Course c = courseService.getCourseByID(courseid, user.getUser_id());
 
             result.put(Constant.status, 1);
             result.put(Constant.result, c);
 
-            return result;
+
 
         } catch (Exception ex) {
             logger.error(ex.getMessage());
-            throw ex;
+            result.put(Constant.status, 0);
+            result.put(Constant.result, ex.getMessage());
         }
+        return result;
     }
 
     @RequestMapping(value = "/getCourseContentListByID", method = RequestMethod.GET)
-    public Map getCourseContentListByID(int courseid) {
-        try {
-            Map<String, Object> result = new HashMap<String, Object>();
+    public Map getCourseContentListByID(int courseid, @RequestHeader Map header) {
+        Map<String, Object> result = new HashMap<String, Object>();
+        String token = header.get("token").toString();
+        User user = logonService.getUserByToken(token);
+        if (user == null) {
+            result.put(Constant.status, 0);
+            result.put(Constant.result, "无效的登录用户");
+            return result;
+        }
 
+        try {
             List<CourseContent> lst = courseService.getCourseContentListByID(courseid);
             result.put(Constant.status, 1);
             result.put(Constant.result, lst);
-            return result;
 
         } catch (Exception ex) {
             logger.error(ex.getMessage());
-            throw ex;
+            result.put(Constant.status, 0);
+            result.put(Constant.result, ex.getMessage());
         }
+        return result;
     }
 
     @RequestMapping(value = "/clickCourseContent", method = RequestMethod.GET)
-    public Map clickCourseContent(int courseid, int contentid) {
-        try {
-            Map<String, Object> result = new HashMap<String, Object>();
+    public Map clickCourseContent(int courseid, int contentid, @RequestHeader Map header) {
 
-            String message = courseService.ClickCourseContent(courseid, contentid, Helper.GetCurrentUser().getUser_id());
+        Map<String, Object> result = new HashMap<String, Object>();
+        String token = header.get("token").toString();
+        User user = logonService.getUserByToken(token);
+        if (user == null) {
+            result.put(Constant.status, 0);
+            result.put(Constant.result, "无效的登录用户");
+            return result;
+        }
+
+        try {
+            String message = courseService.ClickCourseContent(courseid, contentid, user.getUser_id());
             result.put(Constant.status, 1);
             result.put(Constant.result, message);
-            return result;
-
         } catch (Exception ex) {
             logger.error(ex.getMessage());
             throw ex;
         }
+        return result;
     }
 
     //用不到了
     @RequestMapping(value = "/courseCollected", method = RequestMethod.GET)
-    public Map courseCollected(int courseid) {
+    public Map courseCollected(int courseid, @RequestHeader Map header) {
+        Map<String, Object> result = new HashMap<String, Object>();
+        String token = header.get("token").toString();
+        User user = logonService.getUserByToken(token);
+        if (user == null) {
+            result.put(Constant.status, 0);
+            result.put(Constant.result, "无效的登录用户");
+            return result;
+        }
+
         try {
-            Map<String, Object> result = new HashMap<String, Object>();
-            boolean r = courseService.courseCollected(courseid, Helper.GetCurrentUser().getUser_id());
+            boolean r = courseService.courseCollected(courseid, user.getUser_id());
 
             result.put(Constant.status, 1);
             result.put(Constant.result, r);
 
-
-            return result;
-
         } catch (Exception ex) {
             logger.error(ex.getMessage());
-            throw ex;
+            result.put(Constant.status, 0);
+            result.put(Constant.result, ex.getMessage());
         }
+        return result;
     }
 
 
     @RequestMapping(value = "/addCourseCollection", method = RequestMethod.GET)
-    public Map addCourseCollection(int courseid) {
-        try {
-            Map<String, Object> result = new HashMap<String, Object>();
+    public Map addCourseCollection(int courseid, @RequestHeader Map header) {
+        Map<String, Object> result = new HashMap<String, Object>();
+        String token = header.get("token").toString();
+        User user = logonService.getUserByToken(token);
+        if (user == null) {
+            result.put(Constant.status, 0);
+            result.put(Constant.result, "无效的登录用户");
+            return result;
+        }
 
-            boolean r = courseService.courseCollected(courseid, Helper.GetCurrentUser().getUser_id());
+        try {
+
+            boolean r = courseService.courseCollected(courseid, user.getUser_id());
 
             if (r) {
                 result.put(Constant.status, 0);
                 result.put(Constant.result, "不能重复收藏");
 
             } else {
-                collectService.addCollection(Helper.GetCurrentUser().getUser_id(), Helper.GetItemType(1), courseid);
+                collectService.addCollection(user.getUser_id(), Helper.GetItemType(1), courseid);
 
                 result.put(Constant.status, 1);
                 result.put(Constant.result, "收藏成功");
 
             }
-            return result;
 
         } catch (Exception ex) {
             logger.error(ex.getMessage());
-            throw ex;
+            result.put(Constant.status, 1);
+            result.put(Constant.result, ex.getMessage());
         }
+        return result;
     }
 
 
     @RequestMapping(value = "/cancelCourseCollection", method = RequestMethod.GET)
-    public Map cancelCourseCollection(int courseid) {
+    public Map cancelCourseCollection(int courseid, @RequestHeader Map header) {
+        Map<String, Object> result = new HashMap<String, Object>();
+        String token = header.get("token").toString();
+        User user = logonService.getUserByToken(token);
+        if (user == null) {
+            result.put(Constant.status, 0);
+            result.put(Constant.result, "无效的登录用户");
+            return result;
+        }
+
         try {
-            Map<String, Object> result = new HashMap<String, Object>();
-            collectService.cancelCollection(Helper.GetCurrentUser().getUser_id(), Helper.GetItemType(1), courseid);
+            collectService.cancelCollection(user.getUser_id(), Helper.GetItemType(1), courseid);
             result.put(Constant.status, 1);
             result.put(Constant.result, "取消收藏成功");
-            return result;
         } catch (Exception ex) {
             logger.error(ex.getMessage());
-            throw ex;
+            result.put(Constant.status, 1);
+            result.put(Constant.result, ex.getMessage());
         }
+        return result;
     }
 
 
     @RequestMapping(value = "/addCourseComment", method = RequestMethod.POST)
-    public Map addCourseComment(@RequestBody Map body) {
+    public Map addCourseComment(@RequestBody Map body, @RequestHeader Map header) {
+        Map<String, Object> result = new HashMap<String, Object>();
+        String token = header.get("token").toString();
+        User user = logonService.getUserByToken(token);
+        if (user == null) {
+            result.put(Constant.status, 0);
+            result.put(Constant.result, "无效的登录用户");
+            return result;
+        }
 
         try {
-            Map<String, Object> result = new HashMap<String, Object>();
-
-
             int courseid = (int) body.get("courseid");
             int star = (int) body.get("star");
             String comment = (String) body.get("comment");
 
-
-            courseService.addCourseComment(Helper.GetCurrentUser().getUser_id(), courseid, star, comment, new Date());
-
+            courseService.addCourseComment(user.getUser_id(), courseid, star, comment, new Date());
 
             result.put(Constant.status, 1);
             result.put(Constant.result, "评价成功");
 
-            return result;
-
         } catch (Exception ex) {
             logger.error(ex.getMessage());
-            throw ex;
+            result.put(Constant.status, 1);
+            result.put(Constant.result, ex.getMessage());
         }
+        return result;
     }
 
 
     @RequestMapping(value = "/getCourseCommentList", method = RequestMethod.GET)
-    public Map getCourseCommentList(int courseid) {
+    public Map getCourseCommentList(int courseid, @RequestHeader Map header) {
+        Map<String, Object> result = new HashMap<String, Object>();
+        String token = header.get("token").toString();
+        User user = logonService.getUserByToken(token);
+        if (user == null) {
+            result.put(Constant.status, 0);
+            result.put(Constant.result, "无效的登录用户");
+            return result;
+        }
 
         try {
-            Map<String, Object> result = new HashMap<String, Object>();
-
             List<CourseComment> lst = courseService.getCourseCommentList(courseid);
 
             result.put(Constant.status, 1);
             result.put(Constant.result, lst);
 
-            return result;
-
         } catch (Exception ex) {
             logger.error(ex.getMessage());
-            throw ex;
+            result.put(Constant.status, 1);
+            result.put(Constant.result, ex.getMessage());
         }
+        return result;
     }
 
 
     @RequestMapping(value = "/deleteCommentById", method = RequestMethod.GET)
-    public Map deleteCommentById(int commentid) {
+    public Map deleteCommentById(int commentid, @RequestHeader Map header) {
+        Map<String, Object> result = new HashMap<String, Object>();
+        String token = header.get("token").toString();
+        User user = logonService.getUserByToken(token);
+        if (user == null) {
+            result.put(Constant.status, 0);
+            result.put(Constant.result, "无效的登录用户");
+            return result;
+        }
 
         try {
-            Map<String, Object> result = new HashMap<String, Object>();
 
             courseService.deleteCommentById(commentid);
 
             result.put(Constant.status, 1);
             result.put(Constant.result, "删除成功");
 
-            return result;
-
         } catch (Exception ex) {
             logger.error(ex.getMessage());
-            throw ex;
+            result.put(Constant.status, 1);
+            result.put(Constant.result, ex.getMessage());
         }
+        return result;
     }
 }
