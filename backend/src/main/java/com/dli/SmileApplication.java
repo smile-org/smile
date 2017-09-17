2634e3e0bec3fd0678b045e561752cd1142ce9ff
wangqingchen <wangqc@zetyun.com>
2017-09-17 10:50:43 +0800

    add scheduler job.

--- backend/src/main/java/com/dli/SmileApplication.java
+++ backend/src/main/java/com/dli/SmileApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -13,6 +14,7 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
+@EnableScheduling
 public class SmileApplication {
 
     public static void main(String[] args) {
