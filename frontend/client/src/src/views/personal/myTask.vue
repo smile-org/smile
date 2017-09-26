68a65dfbe75f93b7d4ad7bd4616831f33d8c67d0
donghongyu <donghongyu@zhidianlife.com>
2017-09-26 22:49:45 +0800

    course

--- frontend/client/src/src/views/personal/myTask.vue
+++ frontend/client/src/src/views/personal/myTask.vue
@@ -161,7 +161,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped="scope">
   .el-tabs__active-bar{
     width: 0!important;
     transform: translateX(0rem)!important;
--- frontend/client/src/src/views/registration/list.vue
+++ frontend/client/src/src/views/registration/list.vue
@@ -190,7 +190,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped="scope">
 .el-tabs__active-bar {
   width: 0!important;
   transform: translateX(0rem)!important;
