4fd1f48cb6593437205495b8ccd1e60ec77f3705
Cui Lin <lin.cui@yunshipei.com>
2017-10-18 23:58:00 +0800

    创建用户完成

--- frontend/enterprise/src/src/views/user/create.vue
+++ frontend/enterprise/src/src/views/user/create.vue
@@ -7,30 +7,29 @@
         <nav>
           <img src="../../assets/img/house.png" class="vm">
           <span class="vm">您的当前位置 : <span class="f_blue">员工管理</span></span>
-
         </nav>
         <div class="con_tab">
           <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
             <el-form-item label="姓名" prop="name">
               <el-input v-model="ruleForm.name"></el-input>
             </el-form-item>
-            <el-form-item label="手机号" prop="tel">
-              <el-input v-model="ruleForm.tel"></el-input>
+            <el-form-item label="手机号" prop="mobile">
+              <el-input v-model="ruleForm.mobile"></el-input>
             </el-form-item>
-            <el-form-item label="工号" prop="number">
-              <el-input v-model="ruleForm.number"></el-input>
+            <el-form-item label="工号" prop="employeeNo">
+              <el-input v-model="ruleForm.employeeNo"></el-input>
             </el-form-item>
             <el-form-item label="邮箱" prop="email">
               <el-input v-model="ruleForm.email"></el-input>
             </el-form-item>
-            <el-form-item label="部门" prop="apartment">
-              <el-input v-model="ruleForm.apartment"></el-input>
+            <el-form-item label="部门" prop="department">
+              <el-input v-model="ruleForm.department"></el-input>
             </el-form-item>
-            <el-form-item label="区域" prop="adress">
-              <el-input v-model="ruleForm.address"></el-input>
+            <el-form-item label="区域" prop="area">
+              <el-input v-model="ruleForm.area"></el-input>
             </el-form-item>
             <div class="tc">
-              <button class="inf_btn btn_margin" v-on:click="routeByName('informationShow')">保  存</button>
+              <button class="inf_btn btn_margin" type="button" v-on:click="submitForm('ruleForm')">保  存</button>
             </div>
           </el-form>
         </div>
@@ -48,26 +47,32 @@
     data: function () {
       return {
         ruleForm: {
-          company: {},
           name: '',
-          tel: '',
-          number: '',
+          mobile: '',
+          employeeNo: '',
           email: '',
-          apartment: '',
-          address: '',
-          delivery: false,
-          type: [],
-          resource: '',
-          desc: ''
+          department: '',
+          area: ''
         },
         rules: {
           name: [
             { required: true, message: '请输入用户名', trigger: 'blur' },
-            { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+            { min: 2, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
           ],
-          tel: [
+          mobile: [
             { required: true, message: '请输入手机号', trigger: 'change' },
-            { min: 11, max: 12, message: '请输入正确格式的手机号码', trigger: 'blur' }
+            { len: 11, message: '请输入正确格式的手机号码', trigger: 'blur' },
+            { validator: (rule, value, callback) => {
+              if (/^1[34578]\d{9}$/.test(value) === false) {
+                callback(new Error('请输入正确格式的手机号码'))
+              } else {
+                callback()
+              }
+            },
+              trigger: 'blur'}
+          ],
+          email: [
+            { type: 'email', message: '请输入正确的邮箱地址', trigger: 'blur,change' }
           ]
         }
       }
@@ -77,19 +82,23 @@
       navigator
     },
     created () {
-      api.fetch(api.uri.getCompanyInfo).then(data => {
-        if (data.status === 1) {
-          this.company = data
-        }
-      })
     },
     methods: {
       submitForm (formName) {
         this.$refs[formName].validate((valid) => {
           if (valid) {
-            alert('submit!')
+            api.post(api.uri.createUser, {
+              name: this.ruleForm.name,
+              mobile: this.ruleForm.mobile,
+              employeeNo: this.ruleForm.employeeNo,
+              email: this.ruleForm.email,
+              department: this.ruleForm.department,
+              area: this.ruleForm.area
+            }).then(data => {
+              alert('保存成功')
+              this.resetForm(formName)
+            })
           } else {
-            console.log('error submit!!')
             return false
           }
         })
