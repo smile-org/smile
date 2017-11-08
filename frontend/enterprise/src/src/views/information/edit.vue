5c86ec1cc11b6658e432d26891fae28a2f297bc6
donghongyu <donghongyu@zhidianlife.com>
2017-11-08 18:03:09 +0800

    修改预览超出问题

--- frontend/enterprise/src/src/views/information/edit.vue
+++ frontend/enterprise/src/src/views/information/edit.vue
@@ -1,107 +1,107 @@
 <template>
-    <div>
-        <common-header type="information"></common-header>
-        <div class="con_main">
-            <navigator module="information" menu="edit"></navigator>
-            <section class="con_main_r">
-                <nav>
-                    <img src="../../assets/img/house.png" class="vm">
-                    <span class="vm">您的当前位置 : <span class="f_blue">基本信息</span></span>
-                </nav>
-                <div class="con_tab">
-                    <!--&lt;!&ndash;此部分为上传页面&ndash;&gt;-->
-                    <table class="page_m" cellspacing="0" cellpadding="0" border="0">
-                        <tr>
-                            <td class="page_m_a">
-                                个人端logo
-                            </td>
-                            <td class="page_m_b">
-                                <img :src="logoSrc | formatImage" width="20%"/>
-                            </td>
-                            <td class="page_m_c">
-                                <a v-on:click="setDefault('logo')">使用默认</a>
-                                <a v-on:click="toggleShow('logo')">上传</a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td class="page_m_a">
-                                个人端主图
+  <div>
+    <common-header type="information"></common-header>
+    <div class="con_main">
+      <navigator module="information" menu="edit"></navigator>
+      <section class="con_main_r">
+        <nav>
+          <img src="../../assets/img/house.png" class="vm">
+          <span class="vm">您的当前位置 : <span class="f_blue">基本信息</span></span>
+        </nav>
+        <div class="con_tab">
+          <!--&lt;!&ndash;此部分为上传页面&ndash;&gt;-->
+          <table class="page_m" cellspacing="0" cellpadding="0" border="0">
+            <tr>
+              <td class="page_m_a">
+                个人端logo
+              </td>
+              <td class="page_m_b">
+                <img :src="logoSrc | formatImage" width="20%"/>
+              </td>
+              <td class="page_m_c">
+                <a v-on:click="setDefault('logo')">使用默认</a>
+                <a v-on:click="toggleShow('logo')">上传</a>
+              </td>
+            </tr>
+            <tr>
+              <td class="page_m_a">
+                个人端主图
 
-                            </td>
-                            <td class="page_m_b">
-                                <img :src="bannerSrc | formatImage" width="100%"/>
-                            </td>
-                            <td class="page_m_c">
-                                <a v-on:click="setDefault('banner')">使用默认</a>
-                                <a v-on:click="toggleShow('banner')">上传</a>
-                            </td>
-                        </tr>
-                    </table>
-                    <div class="tc">
-                        <button class="inf_btn btn_margin" v-on:click="dialogFormVisible = true">预  览</button>
-                        <el-dialog title="预览" :visible.sync="dialogFormVisible">
-                            <!--&lt;!&ndash;此部分为上传页面&ndash;&gt;-->
-                            <div class="m_show">
-                                <div class="m_show_header">
-                                    <img :src="logoSrc | formatImage"/>
-                                </div>
-                                <!--<img src="img/p_header.jpg"/>-->
-                                <img class="m_show_banner" :src="bannerSrc | formatImage" width="100%"/>
-                            </div>
-                            <div class="tc btn_margin">
-                                <el-button type="success" class="inf_btn  ml20" @click="dialogFormVisible = false">关 闭
-                                </el-button>
-                            </div>
-                        </el-dialog>
-                    </div>
+              </td>
+              <td class="page_m_b">
+                <img :src="bannerSrc | formatImage" width="100%"/>
+              </td>
+              <td class="page_m_c">
+                <a v-on:click="setDefault('banner')">使用默认</a>
+                <a v-on:click="toggleShow('banner')">上传</a>
+              </td>
+            </tr>
+          </table>
+          <div class="tc">
+            <button class="inf_btn btn_margin" v-on:click="dialogFormVisible = true">预  览</button>
+            <el-dialog title="预览" :visible.sync="dialogFormVisible" class="auto_width">
+              <!--&lt;!&ndash;此部分为上传页面&ndash;&gt;-->
+              <div class="m_show">
+                <div class="m_show_header">
+                  <img :src="logoSrc | formatImage"/>
                 </div>
-            </section>
+                <!--<img src="img/p_header.jpg"/>-->
+                <img class="m_show_banner" :src="bannerSrc | formatImage" width="100%"/>
+              </div>
+              <div class="tc btn_margin">
+                <el-button type="success" class="inf_btn  ml20" @click="dialogFormVisible = false">关 闭
+                </el-button>
+              </div>
+            </el-dialog>
+          </div>
         </div>
-        <!--<my-upload @input="closeMyUpload" field="file"
-                   @crop-success="cropSuccess"
-                   @crop-upload-success="cropUploadSuccess"
-                   @crop-upload-fail="cropUploadFail"
-                   :width="width"
-                   :height="height"
-                   :params="params"
-                   :headers="headers"
-                   :value.sync="show"
-                   :no-circle=true
-                   :url="uploadUrl"
-                   img-format="png"></my-upload>-->
-
-        <my-upload
-          @input="closeIcon"
-          field="file"
-          :params="params"
-          @crop-success="cropIconSuccess"
-          @crop-upload-success="cropIconUploadSuccess"
-          @crop-upload-fail="cropIconUploadFail"
-          :url="uploadUrl"
-          :width="280"
-          :headers="headers"
-          :height="194"
-          :value.sync="showIcon"
-          :no-circle=true
-          img-format="png">
-        </my-upload>
-
-        <my-upload
-          @input="closeBanner"
-          field="file"
-          :params="params"
-          @crop-success="cropBannerSuccess"
-          @crop-upload-success="cropBannerUploadSuccess"
-          @crop-upload-fail="cropBannerUploadFail"
-          :url="uploadUrl"
-          :width="375"
-          :headers="headers"
-          :height="120"
-          :value.sync="showBanner"
-          :no-circle=true
-          img-format="png">
-        </my-upload>
+      </section>
     </div>
+    <!--<my-upload @input="closeMyUpload" field="file"
+               @crop-success="cropSuccess"
+               @crop-upload-success="cropUploadSuccess"
+               @crop-upload-fail="cropUploadFail"
+               :width="width"
+               :height="height"
+               :params="params"
+               :headers="headers"
+               :value.sync="show"
+               :no-circle=true
+               :url="uploadUrl"
+               img-format="png"></my-upload>-->
+
+    <my-upload
+      @input="closeIcon"
+      field="file"
+      :params="params"
+      @crop-success="cropIconSuccess"
+      @crop-upload-success="cropIconUploadSuccess"
+      @crop-upload-fail="cropIconUploadFail"
+      :url="uploadUrl"
+      :width="280"
+      :headers="headers"
+      :height="194"
+      :value.sync="showIcon"
+      :no-circle=true
+      img-format="png">
+    </my-upload>
+
+    <my-upload
+      @input="closeBanner"
+      field="file"
+      :params="params"
+      @crop-success="cropBannerSuccess"
+      @crop-upload-success="cropBannerUploadSuccess"
+      @crop-upload-fail="cropBannerUploadFail"
+      :url="uploadUrl"
+      :width="375"
+      :headers="headers"
+      :height="120"
+      :value.sync="showBanner"
+      :no-circle=true
+      img-format="png">
+    </my-upload>
+  </div>
 </template>
 
 <script>
@@ -224,11 +224,15 @@
   }
 </script>
 
-<style  scoped="scope">
-.el-dialog--small {
-  width: auto;
-}
-.page_m{
-  background: #f1f1f1;
-}
+<style scoped="scope">
+
+
+  .page_m {
+    background: #f1f1f1;
+  }
+</style>
+<style>
+  .auto_width .el-dialog--small {
+    width: auto !important;
+  }
 </style>
