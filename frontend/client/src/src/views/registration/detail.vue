7e2375053c727b5345bdb26e27b7ba9f9534c307
donghongyu <donghongyu@zhidianlife.com>
2017-11-19 14:09:41 +0800

    添加讲师

--- frontend/client/src/src/views/registration/detail.vue
+++ frontend/client/src/src/views/registration/detail.vue
@@ -67,7 +67,7 @@
         <el-tab-pane label="课程目录" name="second">
           <ul class="list_border course_con mb3hafe">
             <li v-for="item in courseContent" :key="item.content_id">
-              <p>{{item.sequnce_num}}. {{item.content}}</p>
+              <p>{{item.sequnce_num}}. {{item.content}} <span class="fr">{{item.teacher}}</span></p>
               <p>{{item.sequnce_title | formatDateRange}}</p>
             </li>
           </ul>
